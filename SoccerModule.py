f753b23b305d2a0a8d4cb4be007626a29672b86b
Davinellulinvega <casparluc@gmail.com>
2015-07-23 12:31:57 +0100

    Modify distance calculation between robot and red ball

--- SoccerModule.py
+++ SoccerModule.py
@@ -133,7 +133,7 @@ class SoccerModule(ALModule):
             self.ball[1] = camera_pos[2] * tan(camera_pos[3]) + camera_pos[1]
 
             # Compute the distance between the robot and the ball
-            dist = sqrt(self.ball[0] ** 2 + self.ball[1] ** 2)
+            dist = camera_pos[2] * tan(camera_pos[4] + ball_info[1])
             # Compute the rotation of the ball to the camera center
             rotation = camera_pos[-1] + ball_info[0]
 
