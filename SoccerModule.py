fa8998e5a645f048cef8ee016c0a72b0c075b409
davinellulinvega <casparluc@gmail.com>
2015-07-23 10:36:11 +0100

    Remove useless line of code

--- SoccerModule.py
+++ SoccerModule.py
@@ -61,7 +61,7 @@ class SoccerModule(ALModule):
 		memory.subscribeToEvent("redBallDetected", self.getName(), "on_red_ball")
 		# Subscribe to the robot has fallen event
 		memory.subscribeToEvent("robotHasFallen", self.getName(), "on_fall")
-		#memory.subscribeToEvent("ALMotion/RobotIsFalling", self.getName(), "on_fall")
+
 		# Subscribe to the bumperPressed event
 		memory.subscribeToEvent("RightBumperPressed", self.getName(), "on_bumper_pressed")
 		memory.subscribeToEvent("LeftBumperPressed", self.getName(), "on_bumper_pressed")
